ff412719f4dea8140b95978d28b0c14624364166
Tomson <tomsonj88@wp.pl>
2022-07-24 19:19:06 +0200

    Added tests for PasswordValidator class, added fixture and mock

--- password_validator_test.py
+++ password_validator_test.py
@@ -1,6 +1,5 @@
 import pytest
 from password_validator import PasswordValidator, EmptyPasswordError
-from api import ApiPwnedPasswords
 
 
 def test_empty_password():
@@ -51,38 +50,51 @@ def test_slice_method():
         assert short_hash[element] == personalization_hash[element]
 
 
-password = PasswordValidator("test")
-password2 = PasswordValidator("python")
-resp_api = """FD8D510BFF2210462F26307C2143E990E6E:3
+def test_password_has_been_pwned(requests_mock):
+    password = PasswordValidator("test")
+    data = """
             FDFAEE848356AD27F8FB494E5C1B11956C2:3
             FDFD0D9BC12735B077ACF1FA63D6F42229D:1
             FE5CCB19BA61C4C0873D391E987982FBBD3:86453
             FE872CEF798B9CAD912B101B1FCA6E054C6:1
-            FF36DC7D3284A39991ADA90CAF20D1E3C0D:1
-            FFC7FE7DB601419CF0E1094361291D846C5:1
-            FFF983A91443AE72BD98E59ADAB93B31974:2"""
-
-
-def test_check_password_leakage(requests_mock):
+            """
+    requests_mock.get("https://api.pwnedpasswords.com/range/a94a8", text=data)
     assert password.check_password_leakage() is True
-    assert password2.check_password_leakage() is False
 
 
-# tests for test_is_safe_password & test_is_not_safe_password
-strong = PasswordValidator("ar58#HJkdi")
-api = ApiPwnedPasswords()
-strong_response = """958791E442C6EBD810568CC9D7FA852D797:15
-95DC899DEECC0C29B8789CA143C65F577E9:1
-95F9A1CF60699D487EE90EF3DDDCABF4309:4
-9634FDC1594783EAF03B18675D522B1782B:3
-9777332A7090B57C383DA53A5494A7253BF:16"""
-print(strong_response)
-weak = password
+def test_password_has_not_been_pwned(requests_mock):
+    password = PasswordValidator("python_is_the_anwser")
+    data = """
+            FDFAEE848356AD27F8FB494E5C1B11956C2:3
+            FDFD0D9BC12735B077ACF1FA63D6F42229D:1
+            FE5CCB19BA61C4C0873D391E987982FBBD3:86453
+            FE872CEF798B9CAD912B101B1FCA6E054C6:1
+            """
+    requests_mock.get("https://api.pwnedpasswords.com/range/e470c", text=data)
+    assert password.check_password_leakage() is False
 
 
-def test_is_safe_password():
-    assert strong.is_safe(strong_response) is True
+def test_is_safe_password(requests_mock):
+    strong_pswd = PasswordValidator("ar58#HJkdi")
+    data = """
+            0FD7ED272E8DC9732E9C389AC6503D0C3BC:7
+            0FF326BF498DAD6C732F8A006E56E8BBC4A:1
+            1026E8593048D8969A471EA86BD35FA588F:2
+            106186E54D6664416F354E3F2208A21669C:2
+            """
+    requests_mock.get("https://api.pwnedpasswords.com/range/21ba5", text=data)
+    assert strong_pswd.is_safe() is True
 
 
-def test_is_not_safe_password():
-    assert weak.is_safe(resp_api) is False
+def test_is_not_safe_password(requests_mock):
+    weak_pswd = PasswordValidator("python")
+    data = """
+            2797D4688208BA70C9DCA74D5A922FCECBD:2
+            27AB125768AB71C6F16EDE2EE5C1012C623:2
+            27B51436AD86D07C7CF5D69BDA2644984DE:16202
+            27C6E2C96325CCB9EDCA04203B68D7D5B95:1
+            28379EF3D8C09702DCDBFD8B4F94D3DAB36:1
+            2891F3B1CB433A969D5F54692E5D68F4D77:14
+            """
+    requests_mock.get("https://api.pwnedpasswords.com/range/42352", text=data)
+    assert weak_pswd.is_safe() is False
